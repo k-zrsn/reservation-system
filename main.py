f3da0a085523d37dcae1ed19197962ecd8bbe78d
Virgil Padua <vgcpad@gmail.com>
2024-12-02 14:53:49 -0500

    create avail_tables function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ### Restaurant Reservation System
 
+import time
 
 
 ### Create classes
@@ -7,9 +8,33 @@
 # Table class
 class Table:
     def __init__(self, table_number, capacity):
-        self.table_number = table_number
-        #self.capacity = capacity
-        self.is_available = True
+        self.__table_number = table_number
+        self.__capacity = capacity
+        self.__is_available = True
+
+    @property
+    def table_number(self):
+        return self.__table_number
+    
+    @property
+    def capacity(self):
+        return self.__capacity
+
+    def is_available(self):
+        return self.__is_available
+    
+    def reserve_table(self):
+        if self.__is_available:
+            self.__is_available = False
+            return True
+        return False
+    
+    def __str__(self):
+        status = "Available" if self.__is_available else "Reserved"
+        return f"Table {self.__table_number} (Capacity: {self.__capacity}) - {status}"
+
+
+tables = [Table(i, 4) for i in range(1, 6)]
 
 
 # Customer class
@@ -31,15 +56,34 @@ class Reservation:
 
 
 
+### Create functions
+
+# View available tables
+def avail_tables(tables):
+    print("\nAvailable tables:")
+    for table in tables:
+        if table.is_available:
+            print(f"Table {table.table_number}")
+
+
+
+
 ### Run main program
 def main():
     while True:
-        print("\n\n ---- Restaurant Reservation System ----\n")
+        print("\n\n\n ---- Restaurant Reservation System ---- \n")
         print("1: View available tables")
         print("2: View reservations")
         print("3: Make a reservation")
         print("4: Cancel a reservation")
         print("5: Exit")
+
+        choice = input("\nInput the number of what you would like to do: ")
+
+        if choice == "1":
+            avail_tables(tables)
+            time.sleep(1.5)
+
 
 
 main()
